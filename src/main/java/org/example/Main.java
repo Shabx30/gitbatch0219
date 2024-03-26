3e35e6e9aabd4c326a8c613f59e803ddb3405141
Shabx30 <shabroushan20@gmail.com>
2024-03-26 17:22:07 -0400

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("i love me" );
+        System.out.println(" I love Omar" );
     }
 }
